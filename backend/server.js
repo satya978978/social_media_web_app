e0329f0c1596f455e6d328fd86f2cf3ecea975c1
Satya Yadav <satyaxyadavvv@gmail.com>
2025-06-18 03:36:53 +0530

    server fix

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
-
 require('dotenv').config();
 const app = require('./app');
 const mongoose = require('mongoose');
 
 
 const PORT = process.env.PORT || 5000;
-const MONGODB_URI = process.env.MONGODB_URI || process.env.MONGO_URL; // handles both cases
+const MONGO_URI = process.env.MONGO_URL || process.env.MONGODB_URI;
 
 mongoose.connect(MONGODB_URI)
   .then(() => {
